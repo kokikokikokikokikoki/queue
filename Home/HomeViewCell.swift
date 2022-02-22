631ebff29d90aea047c6a16d0885fbd9a067de7b
koki <buratsakorn.pet@student.mahidol.ac.th>
2022-02-22 11:06:42 +0700

    First test

---
+++ Home/HomeViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewCell.swift
+//  ququququ
+//
+//  Created by Buratsakorn Petchdee on 22/2/2565 BE.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
